cfab887124ba92eafacb6e5ffa11666cc0b126b0
symr1010 <bgzlud@gmail.com>
2020-12-23 16:39:50 +0900

    Revert "Merge pull request #9 from symr1010/second_training"
    This reverts commit dd821bf88a56df3ad0654e2964653f159d325cb0, reversing
    changes made to 5ba69d411de14120747876f8fb474037e8c5ce7c.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_Week
+    get_week
     @plan = Plan.new
   end
 
@@ -15,11 +15,12 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_Week
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -27,24 +28,15 @@ class CalendarsController < ApplicationController
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-    # 今日と今日から6日目までの日付を取得
+
     7.times do |x|
-    # 7回繰り返します  
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-
-      wday_num = (@todays_date+x).wday
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end # 2週間で考えるとif文の必要性が理解できる。
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date+x).wday]}
+      days = { month : (@todays_date + x).month, date : (@todays_date+x).day, plans : today_plans}
       @week_days.push(days)
     end
-    
 
   end
 end
-# [{},{},{}]
